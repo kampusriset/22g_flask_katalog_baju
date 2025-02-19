195906e65daace15cf2913fd56c2e011d0970d03
achaa666 <natashazulatifa@gmail.com>
2025-02-19 21:10:35 +0700

    Add files via upload

---
+++ Team/natasha.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Natasha Zulatifa"
+    nim = "2213010398"
+    peran = "Mengerjakan fitur Penambahan Produk"
+    perkenalan(nama, nim, peran)
---
+++ Team/pani.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Wirdian Silvani"
+    nim = "2213010381"
+    peran = "Mengerjakan fitur Login dan Logout"
+    perkenalan(nama, nim, peran)
